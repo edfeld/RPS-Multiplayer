b9b369675931cb0f800f77b5d273ff04ccc9dc7a
edfeld <edfeld1@gmail.com>
2018-09-23 18:52:34 -0700

    Resolved login. working on game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,7 @@
 RPS-Multiplayer Game
 */
 
+$(document).ready(function() {
 
 
   // Initialize Firebase
@@ -22,27 +23,121 @@ const database = firebase.database();
 database.ref().on("value", function(snapshot) {
     console.log(snapshot.child());
    
-
-    if(snapshot.child("playerOneName").exists() && snapshot.child("playerTwoName").exists()) {
-        console.log("26 PlayerOneName", snapshot.val().playerOneName);
-        console.log("27 PlayerTwoName", snapshot.val().playerTwoName);
+    if(snapshot.child("player").exists()) {
+        console.log("26 ", snapshot.val().player);
         
+        database.ref().once('value', function(snapshot) {
+            console.log("30 ", snapshot.child("player").numChildren());
+            snapshot.child("player").forEach(function(childSnapshot) {
+              var childKey = childSnapshot.key;
+              var childData = childSnapshot.val();
+              console.log("32 childKey", childKey);
+              console.log("33 ", childData);
+              console.log("34 playerName: ", childData.playerName);
+            });
+          });
+            
+        var myArray = database.ref().child("player").orderByChild('playerNumber');
+        console.log("40 ", myArray);
+        console.log("41 ", typeof myArray);
+       
     }
+
+
+    if( snapshot.child("turn").exists()) {
+        plyersLoaded = snapshot.val().turn;
+    } else {
+        plyersLoaded = 0;
+    }
+
+    if (plyersLoaded === 2) {
+        sortie();
+    }
+
+
+    // if(snapshot.child("player").exists() && snapshot.child("playerTwoName").exists()) {
+    //     console.log("30 PlayerOneName", snapshot.val().playerOneName);
+    //     console.log("31 PlayerTwoName", snapshot.val().playerTwoName);
+    // }
 
 }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
 
+function PlayersLoaded () {
+    let plyersLoaded = 0
+    database.ref().once('value', function(snapshot) {
+        if( snapshot.child("turn").exists()) {
+                plyersLoaded = snapshot.val().turn;
+        } else {
+            plyersLoaded = 0;
+        }
+        // snapshot.child("player").exists()
+
+    });
+    return plyersLoaded;
+}
+
 let player= {
-    myPlayerNumber: function(){
+    playerName: "",
+    wins: 0,
+    losses: 0,
+
+    myPlayerNumber: function() {
         if( localStorage.getItem("playerNumber") === undefined){
             return "";
         } else {
             return localStorage.getItem("playerNumber");
         }
     },
-    wins: 0,
-    losses: 0
+}
+
+function sortie () {
+
+    // Here is RPS logic:
+    /*
+    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+      if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
+
+        if ((userGuess === "r") && (computerGuess === "s")) {
+          wins++;
+        } else if ((userGuess === "r") && (computerGuess === "p")) {
+          losses++;
+        } else if ((userGuess === "s") && (computerGuess === "r")) {
+          losses++;
+        } else if ((userGuess === "s") && (computerGuess === "p")) {
+          wins++;
+        } else if ((userGuess === "p") && (computerGuess === "r")) {
+          wins++;
+        } else if ((userGuess === "p") && (computerGuess === "s")) {
+          losses++;
+        } else if (userGuess === computerGuess) {
+          ties++;
+        }
+        */
+}
+
+
+function writeNewPlayer(username, wins, losses, playerNumber) {
+    // a player entry
+    var playerData = {
+        playerName: username,
+        wins: wins,
+        losses: losses,
+        playerNumber: playerNumber
+    };
+    
+    // Get a key for a new Post.
+    var newPlayerKey = database.ref().child('player').push().key;
+    // database.ref().child('player').push([{playerName: username, wins: wins, losses: losses}]);
+    // // Write the new post's data simultaneously in the posts list and the user's post list.
+    
+    var updates = {};
+    updates['/player/' + newPlayerKey] = playerData;
+    
+    // database.ref().child('player').push(updates);
+    return firebase.database().ref().update(updates);
+
 }
 
 var stopwatch = {
@@ -112,26 +207,33 @@ var stopwatch = {
 
 // Whenever a user clicks the "join Game" button for player 1
 $("#player1Join").on("click", function(event) {
+    let playerNum = 0;
     console.log("95 button press")
     // Prevent form from submitting
     event.preventDefault();
+
     
+    let currPlyerLoaded = PlayersLoaded();
+    console.log("174 players loaded: ", currPlyerLoaded);
+
     let txtPlayerOne = $("#player1-name").val();
     // let txtPlayerTwo = $("#player2-name").val();
 
     
-    // if( txtPlayerTwo.trim() === "") { 
-    //     txtPlayerTwo = "empty";
-    // }
-    
     // Test to see if PlayerOneName is empty before updating the database and localstorage
-    if(txtPlayerOne !== "") {
-        localStorage.setItem("playerNumber", 1);
+    if((txtPlayerOne !== "") && currPlyerLoaded < 2 ) {
+        
+        playerNum = currPlyerLoaded + 1;
+
+        localStorage.setItem("playerNumber", playerNum);
+        // database.ref().child("player").child("playerName").push().setValue(txtPlayerOne);
+
+        writeNewPlayer(txtPlayerOne, 0, 0, playerNum); 
         database.ref().update({
-            playerOneName: txtPlayerOne
-        });
+             turn: playerNum
+         });
     }
-    console.log("132 player Number: ", player.myPlayerNumber());
+    console.log("132 player Number: ", playerNum);
 
 });
 
@@ -153,10 +255,17 @@ $("#player2Join").on("click", function(event) {
     if(txtPlayerTwo !== "") {
         localStorage.setItem("playerNumber", 2);
         database.ref().update({
-            playerTwoName: txtPlayerTwo
+            playerTwoName: txtPlayerTwo,
+            playerNumber: "2"
         });
     }
 
     console.log("147 player Number: ", player.myPlayerNumber());
 
 });
+
+// $(window).onbeforeunload(function() {
+//     alert('Handler for .unload() called.');
+// });
+
+}) // $(document).ready(function(){})
